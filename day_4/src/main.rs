8bb39b194ee8831ecc785f4bfdf7f4583226ea4d
Joe Redmon <joe.redmon@emsibg.com>
2025-12-09 09:12:01 -0800

    day 4 print matrix

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,40 @@
 fn main() {
-    println!("Hello, world!");
+    let grid_raw = String::from(
+        "
+..@@.@@@@.
+@@@.@.@.@@
+@@@@@.@.@@
+@.@@@@..@.
+@@.@@@@.@@
+.@@@@@@@.@
+.@.@.@.@@@
+@.@@@.@@@@
+.@@@@@@@@.
+@.@.@@@.@.
+",
+    );
+
+    let m = generate_matrix_from_string(grid_raw);
+    print_matrix(&m);
+}
+
+fn generate_matrix_from_string(raw: String) -> Vec<Vec<char>> {
+    raw.split_whitespace()
+        .map(|l| l.chars().collect())
+        .collect::<Vec<Vec<char>>>()
+}
+
+fn print_matrix<T: std::fmt::Display>(v: &Vec<Vec<T>>) {
+    println!("[");
+    for row in v {
+        print!("   [");
+        for (i, item) in row.iter().enumerate() {
+            if i > 0 {
+                print!(", ");
+            }
+            print!("{}", item);
+        }
+        println!("],");
+    }
+    println!("]");
 }
