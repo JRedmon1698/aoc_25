9c0f65698315e6b32fa7a1def85a7d1405c6d86d
Joe Redmon <joe.redmon@emsibg.com>
2025-12-09 18:30:11 -0800

    day 4 start

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,3 +1,17 @@
+#[derive(Debug, Clone)]
+pub struct PointCell<T> {
+    pub row: usize,
+    pub col: usize,
+    pub value: T,
+}
+
+#[derive(Debug, Clone)]
+pub struct Matrix<T> {
+    rows: usize,
+    cols: usize,
+    cells: Vec<PointCell<T>>,
+}
+
 fn main() {
     let grid_raw = String::from(
         "
@@ -17,6 +31,12 @@ fn main() {
     let m = generate_matrix_from_string(grid_raw);
     print_matrix(&m);
 }
+
+// fn check_adjacent_neighbors<T>(point: Point<T>, grid: &Vec<Vec<T>>) -> bool {
+//     // let
+
+//     false
+// }
 
 fn generate_matrix_from_string(raw: String) -> Vec<Vec<char>> {
     raw.split_whitespace()
